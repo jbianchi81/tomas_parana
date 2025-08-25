0d5e15a2d9b8872a07dc996de7d198a4e3c1b571
jbianchi81 <jbianchi@ina.gob.ar>
2025-08-25 13:34:22 -0300

    import series q

---
+++ import_series_q.py
@@ -0,0 +1,40 @@
+import pandas
+import os
+import re
+from a5client import client
+from datetime import datetime, timedelta
+import json
+
+from pytz import timezone
+
+files = os.listdir("data/series_q")
+
+for file in files:
+    id_externo = re.match(r"\d{8}",file)[0]
+    print("id_externo: %s" % id_externo)
+    series = client.readSeries(id_externo=id_externo, tabla="red_ana_hidro", var_id=40, proc_id=1)
+    if "rows" not in series or not len(series["rows"]):
+        print("No se encontró la serie de caudal medio diario de la estación con id externo = %s" % id_externo)
+        continue
+    serie = series["rows"][0]
+    print("Se encontró la serie id = %i" % serie["id"])
+    # lee csv
+    data = pandas.read_csv(open("data/series_q/%s" % file), sep=r"\s+", names = ["day", "month", "year", "valor"], na_values={"valor":"-1.000000"})
+    # borra nulos
+    data.dropna(inplace=True)
+    # concatena fecha
+    data["timestart"] = pandas.to_datetime(data[["year","month","day"]])
+    data["timestart"] = data["timestart"].dt.tz_localize("America/Argentina/Buenos_Aires", nonexistent="shift_forward")
+    data["series_id"] = serie["id"]
+    data["tipo"] = "puntual"
+    data["timestart"] = data["timestart"].dt.strftime("%Y-%m-%dT%H:%M:%S%z")
+    created = client.createObservaciones(data[["timestart","valor","series_id","tipo"]].to_dict(orient="records"), serie["id"], timeSupport = timedelta(days=1))
+    # created = client.createObservaciones(data[["timestart","valor","series_id","tipo"]], serie["id"], timeSupport = timedelta(days=1))
+    created
+    json.dump(data[["timestart","valor","series_id","tipo"]].to_dict(orient="records"), open("data/series_q_json/%i.json" % serie["id"],"w", encoding="utf-8"), indent=2)
+
+
+
+
+
+
